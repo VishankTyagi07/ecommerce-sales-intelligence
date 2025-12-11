db0e96ab3ea56f22f0eb3c86fefa8d435cb3d427
Vishank Tyagi <tyagivishank1234@gmail.com>
2025-12-11 14:45:08 +0530

    Updated Queries

--- database/queries/Predictive_queries.sql
+++ database/queries/Predictive_queries.sql
@@ -35,7 +35,13 @@ FROM cleaned_sales_data
 GROUP BY customer_id;
 
 --Seasonal demand patterns
-SELECT months, COUNT(order_id) AS orders, SUM(sales) AS sales
+SELECT 
+CASE strftime('%m', order_date)
+        WHEN '01' THEN 'January' WHEN '02' THEN 'February' WHEN '03' THEN 'March'
+        WHEN '04' THEN 'April' WHEN '05' THEN 'May' WHEN '06' THEN 'June'
+        WHEN '07' THEN 'July' WHEN '08' THEN 'August' WHEN '09' THEN 'September' WHEN '10' THEN 'October'
+        WHEN '11' THEN 'November' WHEN '12' THEN 'December'
+    END AS months, COUNT(order_id) AS orders, SUM(sales) AS sales
 FROM cleaned_sales_data
 GROUP BY months
 ORDER BY sales DESC;
